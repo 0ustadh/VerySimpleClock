37e889383c39b38113d155d6baf8bdfa874c43d5
0ustadh <delatorreivanvincent19@gmail.com>
2023-01-25 14:42:09 +0800

    Restore original version for branch 'original'

--- clock.js
+++ clock.js
@@ -17,15 +17,10 @@ class Clock extends React.Component {
     constructor(props) {
       super(props);
       this.state = {date: new Date()};
-      this.changeTime = React.createRef();
-      this.showDate = React.createRef();
     }
 
     componentDidMount() {
-        this.timerID = setInterval(
-            () => this.tick(), 
-            1000
-        );
+        this.timerID = setInterval(() => this.tick(), 1000);
     }
 
     componentWillUnmount() {
@@ -42,31 +37,11 @@ class Clock extends React.Component {
       return (
         <div>
           <h1>Hello! It's</h1>
-          <h2 ref={this.changeTime}>{this.state.date.toLocaleTimeString()}.</h2>
-          <h3 ref={this.showDate}></h3>
-          <button onClick={()=>(alert('24'))}>24 Hours</button>
-          <button onClick={()=>(alert('12'))}>12 Hours</button>
-          <button onClick={()=>(alert('date'))}>Show date</button>
-          <button onClick={()=>startDate()}>Sample</button>
+          <h2>{this.state.date.toLocaleTimeString()}.</h2>
         </div>
       );
     }
   }
-
-  function startDate(){
-    const dates = new Date();
-    let da = dates.getUTCDay();
-    let yr = dates.getFullYear();
-    let mo = dates.getUTCMonth()+1; //month has +1 because Javascript month starts with 0
-    let dy = dates.getUTCDate();
-    mo = checkTime(mo);
-    dy = checkTime(dy);
-    da = days[dates.getUTCDay()];
-    //document.getElementById('date').innerHTML = da +", " + yr + "/" + mo + "/" + dy;
-    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-    return(this.showDate.innerHTML = da +", " + yr + "/" + mo + "/" + dy);
-}
 
 const root = ReactDOM.createRoot(document.getElementById('App'));
 root.render(<App />)
